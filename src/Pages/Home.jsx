c68183e096cad434c0044aa899ac93dd0bbd7358
Yashraj Pravin Desale <yashrajdesale1@gmail.com>
2025-11-14 09:13:43 +0530

    First commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            <div className='text-yellow-800'>
+                Hii from home component
+            </div>
+
+        </div>
+    )
+}
+
+export default Home
